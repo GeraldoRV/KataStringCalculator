255d66a65cdad8bfa651fe8356526ae39f9458d5
LAPTOP-09QGLT5H\Geraldo <geraldo.rodriguesveloso111@gmail.com>
2018-02-04 19:56:34 +0000

    1ª kata

---
+++ test/TestCalculate.java
@@ -0,0 +1,46 @@
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Test
+public class TestCalculate {
+
+    private Calculator calculator;
+
+    @BeforeTest
+    public void init() {
+        calculator = new Calculator();
+    }
+
+    public void emptyStringReturnsZero() throws Exception {
+        assertEquals(calculator.calculate(""), 0);
+    }
+
+    public void singleValueIsReplied() throws Exception {
+        assertEquals(calculator.calculate("1"), 1);
+    }
+
+    public void twoNumbersCommaDelimitedReturnSum() throws Exception {
+        assertEquals(calculator.calculate("1,2"), 3);
+    }
+
+    public void twoNumbersNewLineDelimitedReturnSum() throws Exception {
+        assertEquals(calculator.calculate("1\n2"), 3);
+    }
+
+    public void threeNumbersDelimetedBothWayReturnSum() throws Exception {
+        assertEquals(calculator.calculate("1,2,3"), 6);
+    }
+
+    @Test(expectedExceptions = Exception.class)
+    public void negativeInputReturnsException() throws Exception {
+        calculator.calculate("-1");
+    }
+
+    public void ignoresNumbersGreaterThan1000() throws Exception {
+        assertEquals(calculator.calculate("10,10,1001"), 20);
+    }
+
+}
